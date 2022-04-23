8feb7721f8b020bd2886520d824b923c734d0dc8
Avishek2579 <avishek.1822en1036kiet.edu>
2022-04-23 10:59:39 +0530

    Merge branch 'main' of https://github.com/Avishek2579/M2_Temperature-Control-System

---
+++ 3_Implementation/inc/traffic.h
@@ -0,0 +1,14 @@
+#ifndef TRAFFIC_SIGNAL_SYSTEM_H_
+#define TRAFFIC-SIGNAL-SYSTEM_H
+
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000UL 	
+
+#define LED_PORT (PORTB)    
+#define LED_PIN  (PORTB1)   
+
+#include <avr/io.h>
+
+#endif 
